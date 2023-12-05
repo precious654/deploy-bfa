6e74bb93a87f3e8990e4bf87966900bc36b2a00c
Odukwe Precious <odukwechiemeka@gmail.com>
2023-12-05 17:59:12 +0100

    fc

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
+  server: {
     host: true,
+    strictPort: true,
     port: 8080,
   }
 })
